6ea7364db4f984d83bcae4f41b4269ff70cc4fd6
Семен Корольков <ksan319@gmail.com>
2025-04-22 10:58:58 +0300

    change condition

--- src/main/java/com/pet_projects/bloodspotbotapi/utils/SpotUtils.java
+++ src/main/java/com/pet_projects/bloodspotbotapi/utils/SpotUtils.java
@@ -10,21 +10,22 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 
 public class SpotUtils {
 
-    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.getDefault());
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.getDefault());
 
     public static List<SpotDTO> getSpots(Elements elements) {
         List<SpotDTO> spots = new ArrayList<>();
         for (Element element : elements) {
-            String rawDate = element.selectFirst("td.table-item__date").text();
+            String rawDate = Objects.requireNonNull(element.selectFirst("td.table-item__date")).text();
             LocalDate localDate = LocalDate.parse(rawDate, formatter);
 
-            Element btn = element.selectFirst("td.table-item__btn").getElementsByTag("button").first();
+            Element btn = Objects.requireNonNull(element.selectFirst("td.table-item__btn")).getElementsByTag("button").first();
             if (btn != null) {
                 Attribute attribute = btn.attribute("data-src");
-                if (attribute != null && attribute.getValue().equals("#modal-forbidden")) {
+                if (attribute != null && !attribute.getValue().equals("#modal-forbidden")) {
                     spots.add(SpotDTO.builder().spotDate(localDate).build());
                 }
             }
