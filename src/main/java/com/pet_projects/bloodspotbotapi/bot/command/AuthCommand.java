a3a4f53098290ebf4b669cf1e90c7c8b5096d352
ksan319 <ksan319@gmail.com>
2025-10-13 20:34:44 +0300

    change texts

--- src/main/java/com/pet_projects/bloodspotbotapi/bot/command/AuthCommand.java
+++ src/main/java/com/pet_projects/bloodspotbotapi/bot/command/AuthCommand.java
@@ -3,6 +3,8 @@ package com.pet_projects.bloodspotbotapi.bot.command;
 import com.pet_projects.bloodspotbotapi.bot.handler.MenuDispatcher;
 import com.pet_projects.bloodspotbotapi.bot.keyboard.CustomKeyBoardBuilder;
 import com.pet_projects.bloodspotbotapi.client.TelegramClientWrapper;
+import com.pet_projects.bloodspotbotapi.model.User;
+import com.pet_projects.bloodspotbotapi.repository.UserRepository;
 import com.pet_projects.bloodspotbotapi.service.session.UserState;
 import com.pet_projects.bloodspotbotapi.service.session.UserStateStorage;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class AuthCommand implements BotCommand {
     private final MenuDispatcher menuDispatcher;
     private final UserStateStorage userStateStorage;
+    private final UserRepository userRepository;
 
     @Override
     public String command() {
@@ -30,7 +33,8 @@ public class AuthCommand implements BotCommand {
 
     @Override
     public void process(Long chatId, Update update) throws TelegramApiException {
-        menuDispatcher.sendMenu("auth", chatId, update);
+        User user = userRepository.findById(chatId).get();
+        menuDispatcher.sendMenu("auth", chatId, update, user.getSite());
         userStateStorage.setState(chatId, UserState.AWAITING_AUTH_CREDENTIALS);
     }
 
